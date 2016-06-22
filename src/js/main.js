120063bf5dc1ec44fd98f2354ae948c0f11dd2c9
Brit Butler <redline6561@gmail.com>
2016-06-22 11:04:11 -0400

    Initial commit.

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import cookies from "./second.js";
+import $ from 'jquery';
+
+console.log("JS is automatically compiled and sent to the page on save.");
+console.log("Names is " + cookies);
+
+function sum (x, y) {
+  return x + y;
+};
---
+++ src/js/second.js
@@ -0,0 +1,5 @@
+console.log("This is javascript from another file called second.js");
+
+var names = ["brit", "tony", "terry"];
+
+export default names;
