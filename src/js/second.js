cb5ce990382357673750f7bec76889b278237c81
Brit Butler <redline6561@gmail.com>
2016-06-23 11:14:12 -0400

    Spruce up the page a tiny bit.

--- src/js/second.js
+++ src/js/second.js
@@ -1,5 +1,20 @@
 console.log("This is javascript from another file called second.js");
 
+var numbers = [1,2,3,4];
+
 var names = ["brit", "tony", "terry"];
 
-export default names;
+var dog = {
+  name: 'jurgen',
+  breed: 'everything',
+  mangy: 'definitely',
+  emotions: ['anxious', 'concerned', 'hungry']
+};
+
+var sum = 0;
+// debugger;
+for(var i=0; i < 10; i++) {
+  sum += i;
+}
+
+export {names, numbers, dog as bagel};
