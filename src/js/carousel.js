caa64259dd5fb569b4c6ac24d40e2aa6354c0284
Brit Butler <redline6561@gmail.com>
2016-06-23 11:13:48 -0400

    Review template literals for a minute.

---
+++ src/js/carousel.js
@@ -0,0 +1,39 @@
+var images = [
+  {
+    image_url: 'http://desktopography.net/media/exhibition/2006/hushed/large.jpg',
+    title: 'Hushed',
+    photographer_id: 'Michael Withers'
+  },
+  {
+    image_url: 'http://desktopography.net/media/exhibition/2006/dndln/large.jpg',
+    title: 'Dndln',
+    photographer_id: 'Carlos'
+  },
+  {
+    image_url: 'http://desktopography.net/media/exhibition/2007/open_source/large.jpg',
+    title: 'Open Source',
+    photographer_id: 'Jerico Santander'
+  },
+  {
+    image_url: 'http://desktopography.net/media/exhibition/2007/treehugger/large.jpg',
+    title: 'Treehugger',
+    photographer_id: 'Pete Harrison'
+  },
+  {
+    image_url: 'http://desktopography.net/media/exhibition/2007/dsn/large.jpg',
+    title: 'DSN',
+    photographer_id: 'Marius Bauer'
+  },
+  {
+    image_url: 'http://desktopography.net/media/exhibition/2008/blue_moon/large.jpg',
+    title: 'Blue Moon',
+    photographer_id: 'Microbot'
+  },
+  {
+    image_url: 'http://desktopography.net/media/exhibition/2009/fight_for_alpha/large.jpg',
+    title: 'Fight for Alpha',
+    photographer_id: 'Space Jump'
+  }
+];
+
+export {images};
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,34 @@
-import cookies from "./second.js";
+import {numbers as nums} from "./second.js";
+import {images} from "./carousel";
 import $ from 'jquery';
 
-console.log("JS is automatically compiled and sent to the page on save.");
-console.log("Names is " + cookies);
+console.log('numbers is ' + nums);
+nums[0]++;
+console.log('numbers is now ' + nums);
 
-function sum (x, y) {
-  return x + y;
-};
+function imageTemplate (image) {
+  return `<div class="carousel">
+    <h3 class="image-title">${image.title}</h3>
+    <img src="${image.image_url}">
+    <p>Picture by: ${image.photographer_id}</p>
+  </div>`;
+}
+
+images.forEach(function (image) {
+  var node = $(".board");
+  var imageHTML = imageTemplate(image);
+  node.append(imageHTML);
+});
+
+
+// debugger;
+
+// window.nums = nums;
+
+var sleepy = true;
+
+for(var i = 0; i < 9; i++) {
+  $(".board").append("<div class='space'></div>");
+  // var html = $(".board").html();
+  // $(".board").html(html + "<div class=\"space\">");
+}
